88677b9174fe3ff899791214cd0ce5b65b3f1bb7
Tovah Parnes <tovah@parnes.com>
2024-10-11 10:04:25 +0200

    37 using ping in buckets (#40)
    Full buckets now ping the oldest node (the back of the list) to see if it is awake.. If it is awake the 21 node is discarded, if it is down the old node is discarded and the new node is added to the front of the list. Testing is added.
    
    Issue #39 (Receiving messages adds to routing table) was so small (like 3 lines), so I based this branch in it's branch and will add both to main at the same time.

--- kademlia/bucket_test.go
+++ kademlia/bucket_test.go
@@ -8,7 +8,9 @@ import (
 
 func TestBucket(t *testing.T) {
 	// Create a new bucket
-	bucket := newBucket()
+	contact0 := NewContact(NewKademliaID("0000000000000000000000000000000000000000"), "localhost:8000")
+	network := NewNetwork(contact0, nil)
+	bucket := newBucket(network)
 
 	// Create a new contact
 	contact1 := NewContact(NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "localhost:8000")
@@ -18,18 +20,11 @@ func TestBucket(t *testing.T) {
 	t.Run("Test AddContact", func(t *testing.T) {
 		// Add a contact to the bucket
 		bucket.AddContact(contact1)
+		assert.Equal(t, 1, bucket.Len())
 
-		// Check if the contact has been added to the bucket
-		if bucket.Len() != 1 {
-			t.Fatalf("Expected 1 contact but instead got %d", bucket.Len())
-		}
-
-		// Add another contact to the bucket
 		bucket.AddContact(contact2)
 		bucket.AddContact(contact3)
-		if bucket.Len() != 3 {
-			t.Fatalf("Expected 3 contact but instead got %d", bucket.Len())
-		}
+		assert.Equal(t, 3, bucket.Len())
 	})
 
 	t.Run("Test GetContactAndCalcDistance", func(t *testing.T) {
@@ -41,5 +36,20 @@ func TestBucket(t *testing.T) {
 		assert.Equal(t, contacts[2].distance, contact1.ID)
 		assert.Equal(t, contacts[1].distance, contact2.ID)
 		assert.Equal(t, contacts[0].distance, contact3.ID)
+	})
+
+	t.Run("Test FullBucket", func(t *testing.T) {
+
+		for i := 0; i < bucketSize-3; i++ {
+			bucket.AddContact(NewContact(NewRandomKademliaID(), ""))
+		}
+		assert.Equal(t, bucket.Len(), bucketSize)
+
+		assert.Equal(t, bucket.list.Back().Value.(Contact), contact1)
+
+		newContact := NewContact(NewRandomKademliaID(), "")
+		bucket.AddContact(newContact)
+		assert.Equal(t, bucket.Len(), bucketSize)
+		assert.Equal(t, bucket.list.Front().Value.(Contact), newContact)
 	})
 }
